7e7f56673022d9a0a60d866272cbe4eebce1806e
GlassFish User <gfish@arvi.mgh.harvard.edu>
2014-08-12 20:55:44 -0400

    handles multiples streams

--- stream-server.js
+++ stream-server.js
@@ -1,23 +1,36 @@
 
-if( process.argv.length < 3 ) {
+if( process.argv.length < 2 ) {
 	console.log(
 		'Usage: \n' +
-		'node stream-server.js <secret> [<stream-port> <websocket-port>]'
+		'node stream-server.js [<stream-port> <websocket-port>]'
 	);
 	process.exit();
 }
 
-var STREAM_SECRET = process.argv[2],
-	STREAM_PORT = process.argv[3] || 8082,
-	WEBSOCKET_PORT = process.argv[4] || 8084,
-	STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
 
-var width = 320,
-	height = 240;
+var STREAM_PORT = process.argv[2] || 8082;
+var WEBSOCKET_PORT = process.argv[3] || 8084;
+var STREAM_MAGIC_BYTES = 'jsmp'; // Must be 4 bytes
+var height = [];
+var width = [];
 
-// Websocket Server
-var socketServer = new (require('ws').Server)({port: WEBSOCKET_PORT});
-socketServer.on('connection', function(socket) {
+var httpServer = require('http').createServer(function (req, req) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Not Implemented');
+});
+httpServer.listen(WEBSOCKET_PORT);
+
+var socketServer= {};
+
+function createWebSocketServer(width, height, identifier) {
+    // Websocket Server
+    if(!socketServer[identifier]) {
+	socketServer[identifier] = new (require('ws').Server)({server: httpServer, path: '/'+identifier});
+    }
+    socketServer[identifier].width = width;
+    socketServer[identifier].height = height;
+    
+    socketServer[identifier].on('connection', function(socket) {
 	// Send magic bytes and video size to the newly connected socket
 	// struct { char magic[4]; unsigned short width, height;}
 	var streamHeader = new Buffer(8);
@@ -26,48 +39,40 @@ socketServer.on('connection', function(socket) {
 	streamHeader.writeUInt16BE(height, 6);
 	socket.send(streamHeader, {binary:true});
 
-	console.log( 'New WebSocket Connection ('+socketServer.clients.length+' total)' );
+	console.log( 'New WebSocket Connection ' + identifier + ' ('+socketServer[identifier].clients.length+' total)' );
 	
 	socket.on('close', function(code, message){
-		console.log( 'Disconnected WebSocket ('+socketServer.clients.length+' total)' );
+	    console.log( 'Disconnected WebSocket ' + identifier + ' ('+socketServer[identifier].clients.length+' total)' );
 	});
-});
+    });
 
-socketServer.broadcast = function(data, opts) {
-	for( var i in this.clients ) {
-		if (this.clients[i].readyState == 1) {
-			this.clients[i].send(data, opts);
-		}
-		else {
-			console.log( 'Error: Client ('+i+') not connected.' );
-		}
+    socketServer[identifier].broadcast = function(data, opts) {
+	for( var j in this.clients ) {
+	    if (this.clients[j].readyState == 1) {
+		this.clients[j].send(data, opts);
+	    }
+	    else {
+		console.log( 'Error: Client ('+j+') not connected.' );
+	    }
 	}
-};
-
+    };
+    console.log('Awaiting WebSocket connections on ws://127.0.0.1:'+WEBSOCKET_PORT+'/'+identifier);
+}
 
 // HTTP Server to accept incomming MPEG Stream
 var streamServer = require('http').createServer( function(request, response) {
-	var params = request.url.substr(1).split('/');
-	width = (params[1] || 320)|0;
-	height = (params[2] || 240)|0;
+    var params = request.url.substr(1).split('/');
+    var identifier = params[2];
+    var width = (params[0]||320)|0;
+    var height = (params[1]||240)|0;
 
-	if( params[0] == STREAM_SECRET ) {
-		console.log(
-			'Stream Connected: ' + request.socket.remoteAddress + 
-			':' + request.socket.remotePort + ' size: ' + width + 'x' + height
-		);
-		request.on('data', function(data){
-			socketServer.broadcast(data, {binary:true});
-		});
-	}
-	else {
-		console.log(
-			'Failed Stream Connection: '+ request.socket.remoteAddress + 
-			request.socket.remotePort + ' - wrong secret.'
-		);
-		response.end();
-	}
+    console.log(
+	'Stream Connected: ' + request.socket.remoteAddress + 
+	    ':' + request.socket.remotePort + ' size: ' + width + 'x' + height + ' identifier:' + identifier
+    );	
+    createWebSocketServer(width, height, identifier);
+    request.on('data', function(data){
+	socketServer[identifier].broadcast(data, {binary:true});
+    });
 }).listen(STREAM_PORT);
-
-console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/<secret>/<width>/<height>');
-console.log('Awaiting WebSocket connections on ws://127.0.0.1:'+WEBSOCKET_PORT+'/');
+console.log('Listening for MPEG Stream on http://127.0.0.1:'+STREAM_PORT+'/<width>/<height>/<identifier>');
